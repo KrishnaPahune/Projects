ec6a772b2ce6ded12ba549bc49e39b76c8c62f56
Krishna Pahune <krishnapahune4@gmail.com>
2026-02-01 17:12:16 +0530

    update Random password generater/script.js

--- Random password generater/script.js
+++ Random password generater/script.js
@@ -1,28 +1,34 @@
-let lowercase = 'abcdefghijklnopqrstuvwxyz'
-let uppercase = 'ABCDEFGHIJKLNOPQRSTUVWXYZ'
-let digits = '0123456789'
-let symbol = '!@#$%^&*():"<>?|'
+const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const digits = '0123456789'
+const symbol = '!@#$%^&*():"<>?|' 
 
-let allcharacters = lowercase + uppercase + digits +symbol
+const allCharacters = lowercase + uppercase + digits + symbol
 
+const button = document.querySelector('button')
+const passwordField = document.getElementById('password')
 
-let password =''
-let button = document.querySelector('button')
-let passwordField =document.getElementById('passwor')
+function generatePassword() {
+  let password = ''
 
-password = password + lowercase[Math.floor(Math.random()*lowercase.length)]
-password = password + uppercase[Math.floor(Math.random()*uppercase.length)]
-password = password + digits[Math.floor(Math.random()*digits.length)]
-password = password + symbol[Math.floor(Math.random()*symbol.length)]
+  // ensure at least one char from each set
+  password += lowercase[Math.floor(Math.random() * lowercase.length)]
+  password += uppercase[Math.floor(Math.random() * uppercase.length)]
+  password += digits[Math.floor(Math.random() * digits.length)]
+  password += symbol[Math.floor(Math.random() * symbol.length)]
 
+  // add random chars to reach desired length (10 total here)
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length)
+    password += allCharacters[randomIndex]
+  }
 
-
-for (i=0; i<6; i++){
-    let randomIndex = Math.floor(Math.random()*allcharacters.length)
-    password = password + allcharacters[randomIndex]
+  // shuffle so required chars aren't always at the start
+  return password.split('').sort(() => 0.5 - Math.random()).join('')
 }
-button.addEventListener('click',function(){
-    inputeBox.value = password
 
+button.addEventListener('click', function () {
+  const pwd = generatePassword()
+  passwordField.value = pwd
+  console.log(pwd)
 })
-console.log(password)
